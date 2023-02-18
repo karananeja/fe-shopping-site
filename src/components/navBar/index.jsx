40e0a4210c5bf200742b9fc037aebbe80a748eb8
karananeja <karananeja.r@gmail.com>
2023-02-18 23:56:29 +0530

    added the base structure for the nav bar

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,12 +1,33 @@
+import { Search } from '@mui/icons-material';
 import React from 'react';
 import './NavBar.scss';
 
 const NavBar = () => {
   return (
-    <header>
-      <div className='header__left'>{/* Brand Logo */}</div>
-      <div className='header__center'>{/* Search Bar */}</div>
-      <div className='header__right'>{/* Options */}</div>
+    <header className='header'>
+      <div className='header__left'>
+        {/* Brand Logo */}
+        Brand Logo
+      </div>
+      <div className='header__center'>
+        {/* Search Bar */}
+        <div className='header__searchBar'>
+          <Search />
+          <input
+            type='text'
+            placeholder="Type the name of the product you're looking for"
+          />
+        </div>
+      </div>
+      <div className='header__right'>
+        {/* Options */}
+        <nav className='header__nav'>
+          <ul className='header__unorderedList'>
+            <li>Cart</li>
+            <li>Account</li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
