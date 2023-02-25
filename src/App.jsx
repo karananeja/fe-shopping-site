08e36127a2941a80bd5fe00dc5c1ba2b9e03217d
karananeja <karananeja.r@gmail.com>
2023-02-25 21:54:07 +0530

    updated the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './components/navBar';
 import './App.scss';
+import Cards from './components/cards';
 
 const App = () => {
   return (
     <div className='app'>
       <NavBar />
+      <Cards />
     </div>
   );
 };
