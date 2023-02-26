adb60d0685de803690ec7e61e560326c59bcdb3f
karananeja <karananeja.r@gmail.com>
2023-02-26 22:56:24 +0530

    incorporated react router into the site

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import NavBar from './components/navBar';
 import './App.scss';
 import Cards from './components/cards';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className='app'>
-      <NavBar />
-      <Cards />
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              <NavBar />
+              <Cards />
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 };
