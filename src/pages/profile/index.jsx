2f4aa2702c08e18240c57a98d2e45ac768d90395
karananeja <karananeja.r@gmail.com>
2023-03-19 00:05:53 +0530

    updated the profilePage

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,8 +1,16 @@
+import { BrandingWatermarkOutlined } from '@mui/icons-material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Profile.scss';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className='profile'>
+      <Link to='/'>
+        <BrandingWatermarkOutlined />
+      </Link>
+    </div>
+  );
 };
 
 export default Profile;
