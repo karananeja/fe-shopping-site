606f24247bda521f912ca17ae2730ba956f3f669
karananeja <karananeja.r@gmail.com>
2023-03-25 15:27:06 +0530

    removed inline styling

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -30,36 +30,25 @@ const Product = () => {
       <div className='product__content'>
         {productInfo?.title && (
           <h3 className='product__title'>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>
-              Product Title - {productInfo?.title}
-            </span>
+            <span>Product Title - {productInfo?.title}</span>
           </h3>
         )}
 
         {productInfo?.price && (
           <p className='product__price'>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>
-              Price - {productInfo?.price}
-            </span>{' '}
-            ({productInfo?.loan})
+            <span>Price - {productInfo?.price}</span> ({productInfo?.loan})
           </p>
         )}
 
         {productInfo?.description && (
           <p className='product__description'>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>
-              Description
-            </span>{' '}
-            - {productInfo?.description}
+            <span>Description</span> - {productInfo?.description}
           </p>
         )}
 
         {productInfo?.workingWidth && (
           <p className='product__workingWidth'>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>
-              Working Width
-            </span>{' '}
-            -{' '}
+            <span>Working Width</span> -{' '}
             {productInfo?.workingWidth?.map((width) => (
               <span key={width}>{width}&#34; </span>
             ))}
@@ -68,10 +57,7 @@ const Product = () => {
 
         {productInfo?.shuttleBox && (
           <p className='product__shuttleBox'>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>
-              Shuttle Box
-            </span>{' '}
-            -{' '}
+            <span>Shuttle Box</span> -{' '}
             {productInfo?.shuttleBox?.map((box) => (
               <span key={box}>{box} </span>
             ))}
@@ -80,17 +66,13 @@ const Product = () => {
 
         {productInfo?.shedding && (
           <p className='product__shedding'>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>Shedding</span>{' '}
-            - {productInfo?.shedding}
+            <span>Shedding</span> - {productInfo?.shedding}
           </p>
         )}
 
         {productInfo?.quantityAvailable && (
           <p className='product__quantityAvailable'>
-            <span style={{ color: 'black', fontWeight: 'bold' }}>
-              Available
-            </span>{' '}
-            - {productInfo?.quantityAvailable}{' '}
+            <span>Available</span> - {productInfo?.quantityAvailable}{' '}
             {productInfo?.quantityAvailable > 1 ||
             productInfo?.quantityAvailable === 0
               ? 'pieces'
