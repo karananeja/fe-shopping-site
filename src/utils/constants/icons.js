92ecb04dae53661b14bf1bd98adaa99b4fb91cc5
karananeja <karananeja.r@gmail.com>
2023-04-08 18:49:12 +0530

    added the icons in svg file format

---
+++ src/utils/constants/icons.js
@@ -0,0 +1,23 @@
+import { ReactComponent as AddShoppingCartIconIcon } from '../../assets/icons/add_shopping_cart.svg';
+import { ReactComponent as ArrowCircleLeftIcon } from '../../assets/icons/arrow_circle_left.svg';
+import { ReactComponent as ArrowCircleRightIcon } from '../../assets/icons/arrow_circle_right.svg';
+import { ReactComponent as OpenInBrowserIcon } from '../../assets/icons/open_in_browser.svg';
+import { ReactComponent as BrandLogoIcon } from '../../assets/icons/branding_watermark.svg';
+import { ReactComponent as ShoppingCartIcon } from '../../assets/icons/shopping_cart.svg';
+import { ReactComponent as LoginIcon } from '../../assets/icons/login.svg';
+import { ReactComponent as AccountCircleIcon } from '../../assets/icons/account_circle.svg';
+import { ReactComponent as LightModeIcon } from '../../assets/icons/light_mode.svg';
+import { ReactComponent as LightModeFilledIcon } from '../../assets/icons/light_mode_fill.svg';
+
+export {
+  AccountCircleIcon,
+  AddShoppingCartIconIcon,
+  ArrowCircleLeftIcon,
+  ArrowCircleRightIcon,
+  BrandLogoIcon,
+  LightModeIcon,
+  LightModeFilledIcon,
+  LoginIcon,
+  OpenInBrowserIcon,
+  ShoppingCartIcon,
+};
