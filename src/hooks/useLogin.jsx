6968ec9c538c04453065cd2f340810e4411b23fe
karananeja <karananeja.r@gmail.com>
2023-05-27 22:09:09 +0530

    updated the api methods

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,10 +1,18 @@
 import { useMutation } from 'react-query';
 import { setPassword, signUpUser } from '../infrastructure/network/authConnect';
 
+/**
+ * @summary This method is used to sign up
+ * @param {object} options
+ */
 export const useSignUp = (options = {}) => {
-  return useMutation('user_signup', signUpUser, { ...options });
+  return useMutation(signUpUser, { ...options });
 };
 
+/**
+ * @summary This method is used to set password
+ * @param {object} options
+ */
 export const useSetPassword = (options = {}) => {
-  return useMutation('user_set_password', setPassword, { ...options });
+  return useMutation(setPassword, { ...options });
 };
