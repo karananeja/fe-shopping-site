5c709483442c6f344e32ea7bdb29ccfa3db2f8bd
karananeja <karananeja.r@gmail.com>
2023-06-03 21:59:11 +0530

    removed the local state reference

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Card from '../card';
 import './Cards.scss';
 import data from '../../utils/sampleData.json';
 
 const Cards = () => {
-  const [cardData] = useState(data);
-
   return (
     <div className='cards'>
-      {cardData.map(({ id, imageUrl, title }) => (
+      {data.map(({ id, imageUrl, title }) => (
         <Card key={id} id={id} imageUrl={imageUrl} title={title} />
       ))}
     </div>
