13cfc438633750e1dcfe91dbfe85626255758e5d
karananeja <karananeja.r@gmail.com>
2023-06-04 17:36:19 +0530

    added auth component for basic routing and checks

--- src/routes/authRoutes.jsx
+++ src/routes/authRoutes.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import SignIn from '../pages/userService/signIn';
 import SignUp from '../pages/userService/signUp';
-import SetPassword from '../pages/userService/setPassword';
+import AuthHome from '../pages/userService';
 
 const AuthRoutes = {
   path: 'auth',
+  element: <AuthHome />,
   children: [
     { path: 'signin', element: <SignIn /> },
     { path: 'signup', element: <SignUp /> },
