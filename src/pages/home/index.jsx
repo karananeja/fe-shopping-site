7284a63c481c4f3dd9ccdbf7ebacac41c66805f0
karananeja <karananeja.r@gmail.com>
2023-06-04 23:39:43 +0530

    removed the routing logic from home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,8 @@
-import React, { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import NavBar from '../../components/navBar';
-import { useAuthStatus } from '../../hooks/useAuthStatus';
 
 const Home = () => {
-  const { loggedIn, checkingStatus } = useAuthStatus();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (loggedIn && !checkingStatus) {
-      navigate('/');
-    }
-  }, [loggedIn, checkingStatus, navigate]);
-
   return (
     <>
       <NavBar />
