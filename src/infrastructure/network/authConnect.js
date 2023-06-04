93243dac4360eb2d86ef086dae168a670d350dc8
karananeja <karananeja.r@gmail.com>
2023-06-04 23:38:55 +0530

    updated the api endpoints

--- src/infrastructure/network/authConnect.js
+++ src/infrastructure/network/authConnect.js
@@ -2,7 +2,7 @@ import { apiPost } from './apiClient';
 
 //register user which accepts an email id to send the verification link
 export const signUpUser = (body) => {
-  return apiPost('/user/v1/register', body);
+  return apiPost('/user/register', body);
 };
 
 //set password
@@ -12,10 +12,10 @@ export const setPassword = (body) => {
 
 //forgot Password
 export const forgotPassword = (body) => {
-  return apiPost('/user/v1/password/forgot', body);
+  return apiPost('/user/password/forgot', body);
 };
 
 //sign in user
 export const signInUser = (body) => {
-  return apiPost('/user/v1/sign-in', body);
+  return apiPost('/user/get-token', body);
 };
