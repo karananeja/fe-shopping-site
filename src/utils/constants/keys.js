9455e5e365dcfc5c1bee5fc64329b8d571aa9d99
karananeja <karananeja.r@gmail.com>
2023-06-04 17:34:01 +0530

    updated the keys object

--- src/utils/constants/keys.js
+++ src/utils/constants/keys.js
@@ -3,4 +3,22 @@ export const keys = {
   DATA_MODE: 'data-mode',
   LIGHT: 'light',
   DARK: 'dark',
+  EMAIL: {
+    NAME: 'email',
+    LABEL: 'Email',
+    MESSAGE: 'Please enter your Email!',
+    VALID_MESSAGE: 'Please input a valid email.',
+    REGEX_MESSAGE: 'Your password should meet the password policy.',
+  },
+  PASSWORD: {
+    NAME: 'password',
+    LABEL: 'Password',
+    MESSAGE: 'Please enter your Password!',
+  },
+  CONFIRM_PASSWORD: {
+    NAME: 'confirmPassword',
+    LABEL: 'Confirm Password',
+    MESSAGE: 'Please enter your Password!',
+    ERROR_MESSAGE: 'The two passwords that you entered do not match!',
+  },
 };
