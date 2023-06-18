a21d7dfec86a78ba0f28a822ba06ea3098c7467b
karananeja <karananeja.r@gmail.com>
2023-06-18 23:11:47 +0530

    added a development screen

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import './Cart.scss';
+import { Button, Result } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className='cart'>
+      <Result
+        status='404'
+        subTitle={
+          <>
+            Sorry, the page you visited is under <b>construction/maintenance</b>
+          </>
+        }
+        extra={
+          <Button type='primary' onClick={() => navigate('/')}>
+            Back Home
+          </Button>
+        }
+      />
+    </div>
+  );
 };
 
 export default Cart;
