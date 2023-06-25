c18a055d61ac47d776ab35dc0d83c1459ef1c8f0
karananeja <karananeja.r@gmail.com>
2023-06-25 22:21:35 +0530

    base layout created for profile page

---
+++ src/modules/home/components/settingsContent/index.jsx
@@ -0,0 +1,10 @@
+import { Layout } from 'antd';
+import React from 'react';
+
+const { Content } = Layout;
+
+const SettingsContent = () => {
+  return <Content className='settingsContent'>SettingsContent</Content>;
+};
+
+export default SettingsContent;
