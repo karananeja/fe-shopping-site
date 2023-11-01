4874a65c5e6d86578e0a0a84896d340d446c288e
karananeja <karananeja.r@gmail.com>
2023-11-01 20:32:50 +0530

    used avatar in header

--- src/modules/home/components/navBar/index.jsx
+++ src/modules/home/components/navBar/index.jsx
@@ -14,62 +14,72 @@ import {
   KEYS,
 } from '@utils/constants';
 import { useAuthStatus } from '@modules/auth/hooks/useAuthStatus';
+import { useGetUserInfo } from '@modules/home/hooks/useHome';
+import { Avatar } from 'antd';
 
 const NavBar = () => {
   const [isDarkMode, setIsDarkMode] = useRecoilState(darkModeAtom);
 
   const { loggedIn } = useAuthStatus();
 
+  const { isLoading: isInfoLoading, data: userInfo } = useGetUserInfo({
+    select: (data) => data?.userInfo,
+  });
+
   !isDarkMode
     ? document.documentElement.setAttribute(KEYS.DATA_MODE, KEYS.LIGHT)
     : document.documentElement.setAttribute(KEYS.DATA_MODE, KEYS.DARK);
 
   return (
-    <>
-      <header className='header'>
-        <div className='header__left'>
-          {/* Brand Logo */}
-          <Link to='/'>
-            <BrandLogoIcon />
-          </Link>
+    <header className='header'>
+      <div className='header__left'>
+        {/* Brand Logo */}
+        <Link to='/'>
+          <BrandLogoIcon />
+        </Link>
+      </div>
+      <div className='header__center'>
+        <div className='header__searchBar'>
+          <SearchIcon />
+          <input
+            type='text'
+            placeholder="Search the name of the product you're looking for"
+          />
         </div>
-        <div className='header__center'>
-          <div className='header__searchBar'>
-            <SearchIcon />
-            <input
-              type='text'
-              placeholder="Search the name of the product you're looking for"
-            />
-          </div>
-        </div>
-        <div className='header__right'>
-          {/* Options */}
-          <nav className='header__nav'>
-            <ul className='header__unorderedList'>
-              <li>
-                <Link to='/cart'>
-                  <ShoppingCartIcon />
-                </Link>
-              </li>
-              <li>
-                {loggedIn ? (
-                  <Link to='/profile/details'>
+      </div>
+      <div className='header__right'>
+        {/* Options */}
+        <nav className='header__nav'>
+          <ul className='header__unorderedList'>
+            <li>
+              <Link to='/cart'>
+                <ShoppingCartIcon />
+              </Link>
+            </li>
+            <li>
+              {loggedIn ? (
+                <Link to='/profile/details'>
+                  {isInfoLoading ? (
                     <AccountCircleIcon />
-                  </Link>
-                ) : (
-                  <Link to='/auth/signin'>
-                    <LoginIcon />
-                  </Link>
-                )}
-              </li>
-              <li onClick={() => setIsDarkMode(!isDarkMode)}>
-                {isDarkMode ? <LightModeFilledIcon /> : <LightModeIcon />}
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    </>
+                  ) : (
+                    <Avatar className='header__profile-avatar' size={24}>
+                      {userInfo?.firstName[0]}
+                    </Avatar>
+                  )}
+                </Link>
+              ) : (
+                <Link to='/auth/signin'>
+                  <LoginIcon />
+                </Link>
+              )}
+            </li>
+            <li onClick={() => setIsDarkMode(!isDarkMode)}>
+              {isDarkMode ? <LightModeFilledIcon /> : <LightModeIcon />}
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 
