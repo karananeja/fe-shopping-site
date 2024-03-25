4212d4a9e30e308f1f661e5eeda8cf64036831f9
karananeja <karananeja.r@gmail.com>
2024-03-25 23:02:57 +0530

    removed the dark mode hook ref

--- src/modules/home/components/profileSettings/index.jsx
+++ src/modules/home/components/profileSettings/index.jsx
@@ -5,14 +5,12 @@ import { UserOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
 import { deleteValue } from '@utils/helpers/localStorageManagement';
 import { KEYS } from '@utils/constants';
-import { useDarkMode } from '@hooks/useUtils';
 
 const { Sider } = Layout;
 
 const ProfileMenu = () => {
   const [activeLink, setActiveLink] = useState('details');
   const navigate = useNavigate();
-  useDarkMode();
 
   const items = [
     {
