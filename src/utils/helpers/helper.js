00195a1d9977aef9e5dfb3299e5bb4ef21c7a9bb
karananeja <karananeja.r@gmail.com>
2025-12-27 00:23:02 +0530

    chore: format the currency

--- src/utils/helpers/helper.js
+++ src/utils/helpers/helper.js
@@ -16,3 +16,15 @@ export const displayNotification = ({
   description = '',
   duration = NOTIFICATION_DURATION,
 }) => notification[type]({ message, description, duration });
+
+/**
+ * @summary This method is used to format the currency
+ * @param {number} amount
+ * @returns {string}
+ */
+export const formatCurrency = (amount) => {
+  return new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+  }).format(amount);
+};
