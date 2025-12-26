21ef2c19458d2bfce173df63ea762f83cbd70d82
karananeja <karananeja.r@gmail.com>
2025-12-27 00:22:27 +0530

    chore: code refactor

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,7 @@
-import { createBrowserRouter } from 'react-router-dom';
 import AuthRoutes from '@modules/auth/routes';
-import HomeRoutes from '@modules/home/routes';
 import ErrorRoute from '@modules/error/routes';
+import HomeRoutes from '@modules/home/routes';
+import { createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([AuthRoutes, ...HomeRoutes, ErrorRoute]);
 
